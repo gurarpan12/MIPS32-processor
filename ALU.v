1c39502372fd43b19fce7d84140e825ac2b6bee4
Gurarpan singh <gurarpankurukshetra2@gmail.com>
2026-07-18 13:35:44 +0530

    ALU module

--- ALU.v
+++ ALU.v
@@ -1,24 +1,24 @@
-module alu (
-	input [31:0] a, b,
-	input [2:0] alu_ctrl,
-	output reg [31:0] result,
-	output zero
+module alu(
+    input wire [31:0] a, b,
+    input wire [2:0] alu_ctrl,
+    output reg [31:0] result,
+    output wire zero
 );
-	
-	always @(*) begin
-		case (alu_ctrl)
-			3'b000: result = a & b;
-			3'b001: result = a | b;
-			3'b010: result = a + b;
-			3'b011: result = a ^ b;
-			3'b100: result = ~(a | b);
-			3'b101: result = b << a[4:0];     // SLL
-			3'b110: result = a - b;
-			3'b111: result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; // SLT (Signed)
-			default: result = 32'd0;
-		endcase
-	end
-	
-	assign zero = (result == 32'd0);
-	
+
+    always @(*) begin
+        case (alu_ctrl)
+            3'b000: result = a & b;
+            3'b001: result = a | b;
+            3'b010: result = a + b;
+            3'b011: result = a ^ b;
+            3'b100: result = ~(a | b);
+            3'b101: result = b << a[4:0];
+            3'b110: result = a - b;
+            3'b111: result = ($signed(a) < $signed(b)) ? 32'b1 : 32'b0;
+            default: result = 32'd0;
+        endcase
+    end
+
+    assign zero = (result == 32'd0) ? 1'b1 : 1'b0;
+
 endmodule
