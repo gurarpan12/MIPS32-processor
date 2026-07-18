51367f9f4c2227c1146e1ea6e53d4d388a2c4513
Gurarpan singh <gurarpankurukshetra2@gmail.com>
2026-07-18 13:44:45 +0530

    ex_mem module

--- EX_mem.v
+++ EX_mem.v
@@ -1,5 +1,6 @@
 module ex_mem (
 	input clk,
+	input reset,
 	
 	input [31:0] alu_result_in,
 	input [31:0] write_data_in,
@@ -26,18 +27,32 @@ module ex_mem (
 	output reg branch_out
 );
 
-	always @(posedge clk) begin
-		alu_result_out  <= alu_result_in;
-		write_data_out  <= write_data_in;
-		pc_branch_out   <= pc_branch_in;
-		write_reg_out   <= write_reg_in;
-		zero_out        <= zero_in;
+	always @(posedge clk or posedge reset) begin
+		if (reset) begin
+			alu_result_out <= 32'b0;
+			write_data_out <= 32'b0;
+			pc_branch_out  <= 32'b0;
+			write_reg_out  <= 5'b0;
+			zero_out       <= 1'b0;
 
-		mem_read_out    <= mem_read_in;
-		mem_write_out   <= mem_write_in;
-		mem_to_reg_out  <= mem_to_reg_in;
-		reg_write_out   <= reg_write_in;
-		branch_out      <= branch_in;
+			mem_read_out   <= 1'b0;
+			mem_write_out  <= 1'b0;
+			mem_to_reg_out <= 1'b0;
+			reg_write_out  <= 1'b0;
+			branch_out     <= 1'b0;
+		end else begin
+			alu_result_out  <= alu_result_in;
+			write_data_out  <= write_data_in;
+			pc_branch_out   <= pc_branch_in;
+			write_reg_out   <= write_reg_in;
+			zero_out        <= zero_in;
+
+			mem_read_out    <= mem_read_in;
+			mem_write_out   <= mem_write_in;
+			mem_to_reg_out  <= mem_to_reg_in;
+			reg_write_out   <= reg_write_in;
+			branch_out      <= branch_in;
+		end
 	end
 	
 endmodule
