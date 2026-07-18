311b559777feb316fde1de2ef5fc3a973a2b2676
Gurarpan singh <gurarpankurukshetra2@gmail.com>
2026-07-18 13:48:49 +0530

    imem module

--- imem.v
+++ imem.v
@@ -1,15 +1,21 @@
 module imem (
-	input [31:0] addr,
-	output [31:0] instr
+    input [31:0] addr,
+    output reg [31:0] instr
 );
+    reg [31:0] memory [0:255]; 
 
-	reg [31:0] memory [0:255];
+    initial begin
+        $readmemh("imem_init.mem", memory);
+    end
 
-	initial begin
-		$readmemh("imem_init.mem", memory);
-		$display("IMEM[0] = %h", memory[0]);
-	end
-
-	assign instr = memory[addr[9:2]];  // word-aligned fetch
-
+    always @(*) begin
+        if (addr[9:2] >= 256 || addr[1:0] != 2'b00) begin
+            instr = 32'h00000000; 
+        end else begin
+            instr = memory[addr[9:2]];
+            if (instr === 32'bxxxxxxxx) begin
+                instr = 32'h00000000;
+            end
+        end
+    end
 endmodule
