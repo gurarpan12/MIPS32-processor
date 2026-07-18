18e74a4b095af1e94176e00ef0d8c18f2ee85296
Gurarpan singh <gurarpankurukshetra2@gmail.com>
2026-07-18 13:44:11 +0530

    dmem module

--- dmem.v
+++ dmem.v
@@ -1,24 +1,25 @@
-module dmem (
-	input clk,
-	input mem_read,
-	input mem_write,
-	input [31:0] addr,
-	input [31:0] write_data,
-	output reg [31:0] read_data
+module dmem(
+    input wire clk,
+    input wire mem_read,
+    input wire mem_write,
+    input wire [31:0] addr,
+    input wire [31:0] write_data,
+    output reg [31:0] read_data
 );
 
-	reg [31:0] memory [0:255];
+    reg [31:0] memory [0:255]; // 256 words of memory size is 1024 bytes
 
-	always @(posedge clk) begin
-		if (mem_write)
-			memory[addr[9:2]] <= write_data;
-	end
+    always @(posedge clk) begin
+        if (mem_write) begin
+            memory[addr[9:2]] <= write_data;
+        end
+    end
 
-	always @(*) begin
-		if (mem_read)
-			read_data = memory[addr[9:2]];
-		else
-			read_data = 32'b0;
-	end
-	
+    always @(*) begin
+        if(mem_read)
+            read_data = memory[addr[9:2]];
+        else
+            read_data = 32'b0;
+    end
+    
 endmodule
