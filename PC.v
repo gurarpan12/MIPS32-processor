d473d87a83f6a3075c8f72378f213a6309607363
Gurarpan singh <gurarpankurukshetra2@gmail.com>
2026-07-18 13:49:55 +0530

    PC module

--- PC.v
+++ PC.v
@@ -1,15 +1,16 @@
-module pc (
-	input clk,
-	input reset,
-	input [31:0] next_pc,
-	output reg [31:0] pc
+module pc(
+    input wire clk,
+    input wire reset,
+    input wire [31:0] next_pc,
+    output reg [31:0] pc
 );
-	
-	always @(posedge clk or posedge reset) begin
-		if (reset) 
-			pc <= 32'b0; // Reset PC to 0
-		else
-			pc <= next_pc;
-	end
-	
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            pc <= 32'b0; 
+        end else begin
+            pc <= next_pc;
+        end
+    end
+
 endmodule
